f57971205ba10784f6cb16a64c70aa1196fb6f55
MitaiDart <dartmitai@mail.ru>
2022-12-05 17:33:55 +0500

    fix: locale snippets it's done

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,8 +2,8 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 local luasnip = require('luasnip')
 
-require("luasnip.loaders.from_vscode").lazy_load()
---require("luasnip/loaders/from_vscode").lazy_load{ paths = {"/home/mitai/.config/nvim/lua/snippets"} }
+--require("luasnip.loaders.from_vscode").lazy_load(
+require("luasnip/loaders/from_vscode").lazy_load{ paths = {"/home/mitai/.config/nvim/lua/snippets"} }
 require("luasnip").filetype_extend("dart", { "flutter" })
 
 local source_mapping = {
@@ -52,34 +52,6 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     
-    -- ["<Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expandable() then
-    --     luasnip.expand()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   elseif check_backspace() then
-    --     fallback()
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
   }),
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
